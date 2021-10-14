b95d57062aa0b0fc1389cc9ba78b096b7f711cbd
RooSoft <marc@roosoft.com>
2021-10-14 14:19:53 -0400

    added a manifest and a random icon

--- lib/milk_run_web/endpoint.ex
+++ lib/milk_run_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule MilkRunWeb.Endpoint do
     at: "/",
     from: :milk_run,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images favicon.ico robots.txt manifest.webmanifest apple-touch-icon.png)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
