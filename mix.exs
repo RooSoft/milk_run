cb5b9deec12421e3bb459cb0216659a1307e2e76
RooSoft <marc@roosoft.com>
2021-10-14 14:20:51 -0400

    added the distillery dependency

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule MilkRun.MixProject do
       {:phx_gen_tailwind, "~> 0.1.3", only: :dev},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:websockex, "~> 0.4.3"}
+      {:websockex, "~> 0.4.3"},
+      {:distillery, "~> 2.1"}
     ]
   end
 
