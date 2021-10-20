46d54d9da8443a03ae3e2672413cb7f11d2069cd
RooSoft <marc@roosoft.com>
2021-10-20 15:47:13 -0400

    immediately fetch BTCUSD price before websocket initiation

--- lib/milk_run/clients/bitfinex_client.ex
+++ lib/milk_run/clients/bitfinex_client.ex
@@ -7,16 +7,34 @@ defmodule MilkRun.Clients.Bitfinex do
 
   @stream_endpoint "wss://api.bitfinex.com/ws/1"
 
+  @btccad_ticker_url "https://api-pub.bitfinex.com/v2/ticker/tBTCUSD"
+
   @bitfinex_topic "bitfinex"
   @btcusd_message "btcusd"
 
 
   def start() do
+    broadcast_current_btcusd_price()
+
     WebSockex.start(
       @stream_endpoint,
       __MODULE__,
       %{})
     |> manage_connection
+  end
+
+  def broadcast_current_btcusd_price() do
+    case HTTPoison.get(@btccad_ticker_url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        parse_current_btcusd_price!(body)
+        |> broadcast(@bitfinex_topic, @btcusd_message)
+
+        {:ok}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        {:error, 404}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
   end
 
   def handle_connect(_conn, state) do
@@ -55,6 +73,23 @@ defmodule MilkRun.Clients.Bitfinex do
     {:ok, state}
   end
 
+  defp parse_current_btcusd_price! ticker_body do
+    [
+      _bid,
+      _bid_size,
+      _ask,
+      _ask_size,
+      _daily_change,
+      _daily_change_relative,
+      last_price,
+      _volume,
+      _high,
+      _low
+    ] = Jason.decode!(ticker_body)
+
+    last_price
+  end
+
   defp manage_connection { :ok, pid } do
     { :ok, pid }
   end
@@ -91,17 +126,23 @@ defmodule MilkRun.Clients.Bitfinex do
     formatted_price = :erlang.float_to_binary(data.price, [decimals: 0])
     { int_price, _ } = Integer.parse(formatted_price)
 
-    maybe_broadcast(int_price, topic, message)
+    broadcast(int_price, topic, message)
   end
 
   defp maybe_broadcast(%{ status: :ok } = data, topic, message) when is_integer(data.price) do
-    Cache.set_btcusd(data.price)
-    Endpoint.broadcast(topic, message, data.price)
-
-    data
+    broadcast(data.price, topic, message)
   end
 
-  defp maybe_broadcast _, _, data do
-    data
+  defp maybe_broadcast(%{ status: :nothing }, _, _) do
+    # do nothing... this looks like a keepalive feature not containing any price info
+  end
+
+  defp maybe_broadcast data, _, _ do
+    Logger.warning("NOT broadcasting Bitfinex BTCUSD price because of incompatible format: #{inspect data}")
+  end
+
+  defp broadcast price, topic, message do
+    Cache.set_btcusd(price)
+    Endpoint.broadcast(topic, message, price)
   end
 end
