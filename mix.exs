d9a6e9ddcee2b93cc9b47fee8adaf98bf845b94a
RooSoft <marc@roosoft.com>
2022-05-26 18:33:18 -0400

    bumped version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MilkRun.MixProject do
   def project do
     [
       app: :milk_run,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
