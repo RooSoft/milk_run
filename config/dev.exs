850419c6d348c76617b355170cb4030f3103c96c
roosoft <marc@roosoft.com>
2022-09-29 07:42:11 -0400

    stop logging websocket info

--- config/dev.exs
+++ config/dev.exs
@@ -70,5 +70,7 @@ config :logger, :console,
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
+config :phoenix, :logger, false
+
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
